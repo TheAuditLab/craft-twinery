e945a5a2dec0329b86e0f9006c8d1139fdca0f32
TheAuditLab <analytics@theauditlab.com>
2022-11-01 12:10:53 +0000

    creating three section

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
 <?php   
     include "template-blocks/hero-slider.php";
     include "template-blocks/two-section.php";
+    include "template-blocks/three-section.php";
 ?>
 
 <?php get_footer(); ?>
