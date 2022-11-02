c39c15da7194d50df2a2b6a3b121e5f4259d29e0
TheAuditLab <analytics@theauditlab.com>
2022-11-02 15:02:02 +0000

    updating footer

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
 </main>
 <?php get_sidebar(); ?>
-
-
-
+<footer id="footer">
+    <div class="container">
+        <div class="top-footer">
+            <a href="/">
+                <img class="footer-logo" src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Craft Twinery Logo">
+            </a>
+            <div class="social-icons">
+                <img src="<?php bloginfo('template_url'); ?>/images/facebook-black.svg" alt="Facebook Icon">
+                <img src="<?php bloginfo('template_url'); ?>/images/twitter-black.svg" alt="Twitter Icon">
+            </div>
+            <div class="contact">
+                <div class="phone-number"><a href="tel:01204897642"><span><img src="<?php bloginfo('template_url'); ?>/images/phone-icon.png" alt="Phone Icon">01204 897642</span></a></div>
+                <div class="phone-number"><a href="mailto:info@crafttwinery.co.uk"><span><img src="<?php bloginfo('template_url'); ?>/images/email.svg" alt="Phone Icon">info@crafttwinery.co.uk</span></a></div>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
