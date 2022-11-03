43eec3473546c218f14004cdab026a0fd1285bfc
TheAuditLab <analytics@theauditlab.com>
2022-11-03 13:23:01 +0000

    updating footer

--- footer.php
+++ footer.php
@@ -3,16 +3,16 @@
 <footer id="footer">
     <div class="container">
         <div class="top-footer">
-            <a href="/">
+            <a href="/" class="logo-div">
                 <img class="footer-logo" src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Craft Twinery Logo">
             </a>
             <div class="social-icons">
-                <img src="<?php bloginfo('template_url'); ?>/images/facebook-black.svg" alt="Facebook Icon">
-                <img src="<?php bloginfo('template_url'); ?>/images/twitter-black.svg" alt="Twitter Icon">
+                <img src="<?php bloginfo('template_url'); ?>/images/facebook.png" alt="Facebook Icon">
+                <img src="<?php bloginfo('template_url'); ?>/images/twitter.png" alt="Twitter Icon">
             </div>
             <div class="contact">
                 <div class="phone-number"><a href="tel:01204897642"><span><img src="<?php bloginfo('template_url'); ?>/images/phone-icon.png" alt="Phone Icon">01204 897642</span></a></div>
-                <div class="phone-number"><a href="mailto:info@crafttwinery.co.uk"><span><img src="<?php bloginfo('template_url'); ?>/images/email.svg" alt="Phone Icon">info@crafttwinery.co.uk</span></a></div>
+                <div class="email"><a href="mailto:info@crafttwinery.co.uk"><span><img src="<?php bloginfo('template_url'); ?>/images/email.png" alt="Email Icon">info@crafttwinery.co.uk</span></a></div>
             </div>
         </div>
     </div>
