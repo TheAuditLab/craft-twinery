909ad00517aed566146bcd865475ea35108d9d9d
TheAuditLab <analytics@theauditlab.com>
2022-11-03 13:28:07 +0000

    adding link to social icons

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
                 <img class="footer-logo" src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Craft Twinery Logo">
             </a>
             <div class="social-icons">
-                <img src="<?php bloginfo('template_url'); ?>/images/facebook.png" alt="Facebook Icon">
-                <img src="<?php bloginfo('template_url'); ?>/images/twitter.png" alt="Twitter Icon">
+                <a href="https://www.facebook.com/crafttwinery/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/facebook.png" alt="Facebook Icon"></a>
+                <a href="https://twitter.com/CraftTwinery" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/twitter.png" alt="Twitter Icon"></a>
             </div>
             <div class="contact">
                 <div class="phone-number"><a href="tel:01204897642"><span><img src="<?php bloginfo('template_url'); ?>/images/phone-icon.png" alt="Phone Icon">01204 897642</span></a></div>
