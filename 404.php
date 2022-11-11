007d2a5c1ab88f8f022e7e0b1b7fe035f1d3a8f7
TheAuditLab <analytics@theauditlab.com>
2022-11-11 09:48:46 +0000

    adding 404 page and updating styling on website

--- 404.php
+++ 404.php
@@ -2,12 +2,13 @@
 <div class="container">
     <article id="post-0" class="post not-found">
     <header class="header">
-    <h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Not Found', 'generic' ); ?></h1>
+    <h1 class="entry-title" itemprop="name"><?php esc_html_e( '404 ', 'generic' ); ?></h1>
     </header>
     <div class="entry-content" itemprop="mainContentOfPage">
-    <p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'generic' ); ?></p>
+    <p><?php esc_html_e( 'The page you are looking for does not exist. Try a search instead?', 'generic' ); ?></p>
     <?php get_search_form(); ?>
     </div>
     </article>
 </div>
+<?php include "template-blocks/account-block.php";  ?>
 <?php get_footer(); ?>
